46862d06629f3d2deb7f846f3d6e5a24f831bcb8
Kenji Saito <ken-yo@mbr.nifty.com>
2025-04-20 16:04:27 +0900

    Add the template

---
+++ update.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}" || exit
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+echo ""
+pwd
+npx pnpm@latest self-update && pnpm install && pnpm up && pnpm lint-fix && pnpm build
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+git commit -am "Bumps node modules" && git push
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CUR}" || exit
