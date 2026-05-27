4cab571d8d468e50ad8bc8f4e4c7cb53f6e1e8f1
Kenji Saito <ken-yo@mbr.nifty.com>
2026-05-27 23:35:10 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -20,11 +20,9 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-npx -y pnpm@latest self-update && pnpm install && pnpm up -r && pnpm audit --fix && pnpm up -r  && pnpm -r lint-fix && pnpm -r build
-result=$?
-if [ $result -ne 0 ]; then
+if ! (disable-checkout-persist-credentials && pnx pnpm@latest self-update && pnpm install --no-frozen-lockfile && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm -r --if-present lint-fix && pnpm -r --if-present build && pnpm install --no-frozen-lockfile); then
   cd "${CUR}" || exit
-  exit $result
+  exit 1
 fi
 
 cd "${CURRENT}" || exit
